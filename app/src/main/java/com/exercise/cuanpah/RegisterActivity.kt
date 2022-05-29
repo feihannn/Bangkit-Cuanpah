a09cf272d25c6db22d31c464709f82f19226e09b
feihannn <marcelino.feihan@gmail.com>
2022-05-29 15:18:50 +0700

    register Function

--- app/src/main/java/com/exercise/cuanpah/RegisterActivity.kt
+++ app/src/main/java/com/exercise/cuanpah/RegisterActivity.kt
@@ -1,11 +1,27 @@
 package com.exercise.cuanpah
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.exercise.cuanpah.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding= ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toLoginRegister.setOnClickListener {
+            val i= Intent(this,LoginActivity::class.java)
+            startActivity(i)
+        }
+
+        binding.buttonRegister.setOnClickListener {
+            Toast.makeText(this,"Register Berhasil",Toast.LENGTH_SHORT).show()
+            val i= Intent(this,LoginActivity::class.java)
+            startActivity(i)
+        }
     }
 }
