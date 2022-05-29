75fe2136192b7a97eccd10a18065b689abe4b4f4
Nicholas <nicholasnick230@gmail.com>
2022-05-29 13:36:54 +0700

    layout login

--- app/src/main/java/com/exercise/cuanpah/LoginActivity.kt
+++ app/src/main/java/com/exercise/cuanpah/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.exercise.cuanpah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.exercise.cuanpah.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
