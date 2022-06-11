054270b6a07d986135f71a294ce208fc3aed7c7c
Nicholas <nicholasnick230@gmail.com>
2022-06-11 18:29:40 +0700

    profil

--- app/src/main/java/com/exercise/cuanpah/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/exercise/cuanpah/ui/profile/ProfileFragment.kt
@@ -1,14 +1,26 @@
 package com.exercise.cuanpah.ui.profile
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.exercise.cuanpah.data.UserPreference
 import com.exercise.cuanpah.databinding.FragmentProfileBinding
+import com.exercise.cuanpah.ui.SplashActivity
+import com.exercise.cuanpah.ui.ViewModelFactory
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var profileViewModel: ProfileViewModel
     private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
@@ -22,11 +34,29 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        setupAction()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupAction() {
+        profileViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreference.getInstance(requireContext().dataStore), "")
+        )[ProfileViewModel::class.java]
+
+        profileViewModel.getUser().observe(requireActivity()) {
+            binding.profileName.text = it.name
+        }
+
+        binding.logoutButton.setOnClickListener {
+            profileViewModel.logout()
+            startActivity(Intent(requireContext(), SplashActivity::class.java))
+        }
+
     }
 }
